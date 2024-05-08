ef0d4f87b31192410ea9c5a3a043e40f66546e0e
Aymo <96393445+NotAymo@users.noreply.github.com>
2024-05-08 14:23:04 +0200

    fd

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
     <head>
         <title>Onyx - Homepage</title>
         <link rel="stylesheet" href="onyx.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap" rel="stylesheet">
     </head>
     <body>
         <h1>Welcome to Onyx, an open-source social media made by trust</h1>
