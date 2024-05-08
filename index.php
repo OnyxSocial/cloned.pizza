101cc00ea18803d26403abbc9d8d0d7d45e10b3d
Aymo <96393445+NotAymo@users.noreply.github.com>
2024-05-08 14:17:20 +0200

    zddfs

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-
+<!doctype html>  
+<html>
+    <head>
+        <title>Onyx - Homepage</title>
+        
+    </head>
+    <body>
+        <h1>Welcome to Onyx, an open-source social media made by trust</h1>
+        <p>We, at Onyx, want our users to have trust in open-source software/services.</p>
+        <button>Join Onyx</button>
+    </body>
+</html>
